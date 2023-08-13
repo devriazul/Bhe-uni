8fdfb5af137db49ffcbcba758bd7551700584548
devriazul <engr.riazul@gmail.com>
2023-08-13 17:07:25 +0600

    Why choose updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+{{-- Hero Section Start --}}
     <div class="container">
         <div class="row py-3">
             <div class="col-md-6 d-flex align-items-center">
@@ -16,13 +17,16 @@
                 </div>
                 <div class="row">
                     <div class="d-flex justify-content-center">
-                        <img src="{{ asset('front/img/home/Mobile app store badge.png') }}" class="img-fluid pe-3" alt="...">
-                        <img src="{{ asset('front/img/home/Mobile app store badge-1.png') }}" class="img-fluid" alt="...">
+                        <a href=""><img src="{{ asset('front/img/home/Mobile app store badge.png') }}" class="img-fluid pe-3" alt="..."></a>
+                        <a href=""><img src="{{ asset('front/img/home/Mobile app store badge-1.png') }}" class="img-fluid" alt="..."></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+{{-- Hero Section End --}}
+
+{{-- Search Section Start --}}
     <div class="search-section">
         <div class="py-5">
 
@@ -83,4 +87,159 @@
             </div>
         </div>
     </div>
+{{-- Search Section End --}}
+
+{{-- Logo Section Start --}}
+    <div class="container">
+        <h4 class="text-center fw-bold primary-color py-3"><span class="secondary-color">Trusted</span> by <span class="secondary-color">300</span> Top Ranked Universities</h4>
+        <div class="row">
+            <div class="col-md-2">
+                <img class="img-fluid mb-5" src="{{ asset('front/img/home/Frame 152.png') }}"  alt="">
+            </div>
+            <div class="col-md-10">
+                <div class="lc-block">
+                    <div id="carouselLogos" class="carousel slide my-3" data-bs-ride="carousel">
+                        <div class="carousel-inner px-5">
+                            <div class="carousel-item active">
+                                <div class="row">
+                                    <div class="col-6 col-lg-2 align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/aru-2.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/Keela-logo.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/Kings-Edu-for-life.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/Kins-Stage-B-S.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/Malvern-house-london.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/Nord_Anglia_Education.webp') }}" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+                                    <div class="col-6 col-lg-2 align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/North-London-Grammar-School.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/oxford-logo.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/Study-Group.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/UCA.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/uclan-logo.webp') }}" alt="">
+                                    </div>
+                                    <div class="col-6 col-lg-2  align-self-center">
+                                        <img class="img-fluid d-block w-100 px-4 py-2 mb-3 bg-w border rounded-3" src="{{ asset('front/img/uni-logo/Ravensbourne-Uni.webp') }}" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{{-- Search Section End --}}
+
+{{-- Schoolarship section start --}}
+<div class="secondary-light-background">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ asset('front/img/home/Rectangle 7009.png') }}" class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-6 d-flex align-items-center">
+                <div>
+                    <h5 class="secondary-color fw-bold">Scholarship</h5>
+                    <h3 class="primary-color fw-bold">and Funding</h3>
+                    <p>BHE Uni, we are dedicated to helping students worldwide achieve their dreams of studying in prestigious universities in the UK, USA, Canada and Australia. Through our comprehensive scholarship and funding services, we provide specific scholarships and bursaries...</p>
+                    <button>Read More</button>
+                    <button>Make Enquiry</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- Schoolarship section End --}}
+
+{{-- Book & Accommodation section Start --}}
+    <div class="container my-md-5">
+        <div class="row">
+            <div class="col-md-6 d-flex justify-content-center px-5 py-4 py-md-0">
+                <div class="card border-0 shadow">
+                    <img src="{{ asset('front/img/home/Rectangle 6994.png') }}" class="card-img-top img-fluid" alt="..." style="max-height: 200px;">
+                    <div class="card-body mx-3">
+                        <p class="card-text secondary-color py-2 fw-bold">Flight  Booking</p>
+                        <h5 class="card-title primary-color pb-2 fw-bold">Effortless Flight Booking: Simplified <br> and Seamless</h5>
+                        <button class="my-3">Book Your Flight</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center px-5 py-4 py-md-0">
+                <div class="card border-0 shadow">
+                    <img src="{{ asset('front/img/home/Rectangle 6994-1.png') }}" class="card-img-top img-fluid" alt="..." style="max-height: 200px;">
+                    <div class="card-body mx-3">
+                        <p class="card-text secondary-color py-2 fw-bold">Accommodations</p>
+                        <h5 class="card-title primary-color pb-2 fw-bold">Seamless Accommodation Booking: Effortlessly Reserve Your Stay</h5>
+                        <button class="my-3">Book Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{{-- Book & Accommodation section End --}}
+
+{{-- Why Choose section Start --}}
+    <div class="secondary-light-background">
+        <div class="container pb-5">
+            <div class="row py-5">
+                <div class="col-md-6">
+                    <div class="secondary-color">
+                        <h2 class="fw-bold py-3">Why Choose BHE?</h2>
+                        <p><img src="{{ asset('front/img/home/icon-tik.png') }}" alt=""> Established: Our Journey started in 2012 and served over 30,000 applicants for their higher education guidance.</p>
+                        <p><img src="{{ asset('front/img/home/icon-tik.png') }}" alt=""> ‘Check Eligibility tool’ Powered by AI: Experience a seamless and efficient application journey with our cutting-edge Artificial Intelligence tools, simplifying the process and saving you valuable time.</p>
+                        <p><img src="{{ asset('front/img/home/icon-tik.png') }}" alt=""> One-2-One Consultations: Connect with world-class experts for tailored guidance, ensuring you receive prompt responses and making well-informed decisions about your academic path. In Addition, prepare confidently for interviews and Pre-CAS interviews with our dedicated YouTube channel and mobile app, providing comprehensive interview prep and CAS guidance.</p>
+                        <p><img src="{{ asset('front/img/home/icon-tik.png') }}" alt=""> Fast-Truck Offer Letters: No more waiting anxiously! Get your offer letter within 24-48 hours, securing your place at your dream university without delays. It’s only for specific University.</p>
+                        <p><img src="{{ asset('front/img/home/icon-tik.png') }}" alt=""> Extensive Social Media Presence: Stay connected and inspired through all major social media platforms, where we share valuable updates on scholarships and motivational content.</p>
+                        <p><img src="{{ asset('front/img/home/icon-tik.png') }}" alt=""> Online ILETS or English test preparation: We provide video content to prepare for English tests such as IELTS, TOFEL, PTE and Duolingo.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-items-center">
+                    <img src="{{ asset('front/img/home/Vvector Smart Object 1.png') }}" class="img-fluid" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container px-5 mb-5" style="margin-top: -70px">
+        <div class="px-5">
+            <div class="card highligh-secondary text-center py-5">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h2 class="fw-bold">4000+</h2>
+                        <h6>Course</h6>
+                    </div>
+                    <div class="col-md-4 border-left">
+                        <h2 class="fw-bold">75+</h2>
+                        <h6>UK University</h6>
+                    </div>
+                    <div class="col-md-4 border-left">
+                        <h2 class="fw-bold">5000+</h2>
+                        <h6>Admission Success</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{{-- Why Choose section End --}}
 @endsection
